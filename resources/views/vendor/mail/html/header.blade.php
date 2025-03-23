20a3956b7a05d7d9a1874f01de849f828ffd555b
Adi Asmawi <23FTT1123@student.pb.edu.bn>
2025-03-23 23:23:44 +0800

    logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="pblogo.png" alt="Your Logo">
+<img src="{{ asset('pblogo.png') }}" class="logo" alt="Your Logo">
 @else
 {{ $slot }}
 @endif
